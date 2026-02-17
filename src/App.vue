a761e17cf4d3e99d27964a8a6d360727a609ac18
abineshcse699-design <abineshcse699@gmail.com>
2026-02-17 12:03:11 +0530

    first committ2

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,47 @@
 <template>
-  <h1>{{ message }}</h1>
+  <main class="container">
+    <h1>{{ message }}</h1>
 
-  <TaskForm @add-task="addTask" />
+    <TaskForm @add-task="addTask" />
 
-  <h3 v-if="!tasks.length">Add a task to get started</h3>
-  <h3 v-else>{{ totalDone }} / {{ tasks.length }} task completed</h3>
+    <h3 v-if="!tasks.length">Add a task to get started</h3>
+    <h3 v-else>{{ totalDone }} / {{ tasks.length }} task completed</h3>
 
-  <div v-if="tasks.length" class="button-container1">
-    <FilterButton
-      :currentFilter="filter"
-      filter="all"
-      @set-filter="setFilter"
+    <div v-if="tasks.length" class="button-container1">
+      <FilterButton
+        :currentFilter="filter"
+        filter="all"
+        @set-filter="setFilter"
+      />
+      <FilterButton
+        :currentFilter="filter"
+        filter="todo"
+        @set-filter="setFilter"
+      />
+      <FilterButton
+        :currentFilter="filter"
+        filter="done"
+        @set-filter="setFilter"
+      />
+    </div>
+
+    <Tasklist
+      :tasks="filteredTasks"
+      @toggle-done="toggleDone"
+      @remove-task="removeTask"
     />
-    <FilterButton
-      :currentFilter="filter"
-      filter="todo"
-      @set-filter="setFilter"
-    />
-    <FilterButton
-      :currentFilter="filter"
-      filter="done"
-      @set-filter="setFilter"
-    />
-  </div>
-
-  <Tasklist
-    :tasks="filteredTasks"
-    @toggle-done="toggleDone"
-    @remove-task="removeTask"
-  />
+  </main>
 </template>
 
 <script setup lang="ts">
 import { computed, ref } from 'vue'
 import TaskForm from '@/components/TaskForm.vue'
 import Tasklist from '@/components/Tasklist.vue'
-import FilterButton from './components/FilterButton.vue'
+import FilterButton from '@/components/FilterButton.vue'
 
+import type { Task, TaskFilter } from '@/types'
 
-import type { Task, TaskFilter } from './types'
-
-const message = ref('hello world')
+const message = ref('Hello World')
 const tasks = ref<Task[]>([])
 const filter = ref<TaskFilter>('all')
 
@@ -71,16 +72,11 @@ function addTask(newTask: string) {
 
 function toggleDone(id: string) {
   const task = tasks.value.find(task => task.id === id)
-  if (task) {
-    task.done = !task.done
-  }
+  if (task) task.done = !task.done
 }
 
 function removeTask(id: string) {
-  const index = tasks.value.findIndex(task => task.id === id)
-  if (index !== -1) {
-    tasks.value.splice(index, 1)
-  }
+  tasks.value = tasks.value.filter(task => task.id !== id)
 }
 
 function setFilter(value: TaskFilter) {
@@ -88,25 +84,49 @@ function setFilter(value: TaskFilter) {
 }
 </script>
 
-
-<style >
-main{
-    max-width: 800px;
-    margin:1rem-auto;
+<style>
+body {
+  margin: 0;
+  font-family: Arial, sans-serif;
 }
+
+.container {
+  width: 80%;
+  max-width: 1200px;
+  margin: 2rem auto;
+  padding: 1rem;
+}
+
+h1 {
+  text-align: center;
+  margin-bottom: 2rem;
+}
+
 .button-container{
-  display: flex;
-  /* justify-content: end; */
-  gap: 20px;
   width: 200px;
-  margin-left: 85%;
+  margin-left: 80%;
 }
-.button-container1{
+
+.button-container1 {
   display: flex;
-  justify-content: end;
+  justify-content: flex-end;
   gap: 20px;
-  margin-right: 20px;
+  margin: 1rem 0;
+  flex-wrap: wrap;
 }
 
+/* Responsive */
+@media (max-width: 768px) {
+  .container {
+    width: 95%;
+  }
 
+  .button-container1 {
+    justify-content: center;
+  }
+  .button-container{
+    justify-content: center;
+    margin-left: 60%;
+  }
+}
 </style>
